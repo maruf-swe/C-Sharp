c4025055918ed7e8f5b0c7c5d2ea4ac2ea12e53a
Maruf <marufur1847@gmail.com>
2021-08-15 23:40:31 +0600

    variable files add

---
+++ variable/variable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Maruf";
+            int age = 24;
+            Console.WriteLine("There was a man name " +name);
+            Console.WriteLine("His age is "+age);
+        }
+    }
+}
