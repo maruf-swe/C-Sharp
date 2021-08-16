c7b1929b27835717a1f6ea01ad9441326d3150b8
Maruf <marufur1847@gmail.com>
2021-08-16 12:01:19 +0600

    add string program

--- 03_Data_Types/03_Data_Types/Program.cs
+++ 03_Data_Types/03_Data_Types/Program.cs
@@ -6,7 +6,14 @@ namespace _03_Data_Types
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Maruf";
+            char names_start_with = 'M'; // for single character
+            int age = 24;
+            double gpa = 3.0;
+            bool isMale = true;
+
+
+
         }
     }
 }
---
+++ 04_Working_With_Strings/04_Working_With_Strings/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _04_Working_With_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Examples of Special Characters
+            Console.WriteLine("Hello\nWorld!"); // line break
+            Console.WriteLine("It\'s World!"); // single quote
+            Console.WriteLine("Hello\"s World!"); // double quote
+            Console.WriteLine("\"Hello\"s World!"); // double quote
+            Console.WriteLine("Hello\\World!"); // backslash
+
+            // return boolean
+
+            string x = "Maruf Academy";
+            Console.WriteLine(x.Contains("Academy"));
+
+            // string length
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("The length of the txt string is: " + txt.Length);
+
+            // string method
+            string txt2 = "Hello World";
+            Console.WriteLine(txt2.ToUpper());   // Outputs "HELLO WORLD"
+            Console.WriteLine(txt2.ToLower());   // Outputs "hello world"
+
+            // string concetation
+            string firstName = "John ";
+            string lastName = "Doe";
+            string name = firstName + lastName;
+            Console.WriteLine(name);
+
+            // String Interpolation
+            string fName = "John";
+            string lName = "Doe";
+            string name2 = $"My full name is: {fName} {lName}";
+            Console.WriteLine(name2);
+
+            // access string
+            string a = "Hello World";
+            Console.WriteLine(a[6]);
+            Console.WriteLine(a.IndexOf("l"));
+
+            // slice string
+            string y = "Hello Maruf";
+            Console.WriteLine(y.Substring(6));
+            Console.WriteLine(y.Substring(6,2));
+       
+
+
+        }
+    }
+}
