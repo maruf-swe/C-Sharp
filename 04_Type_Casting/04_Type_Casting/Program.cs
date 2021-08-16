44690d500a8eb053de0674f0d4e8f1f2bb698de6
Maruf <marufur1847@gmail.com>
2021-08-16 12:05:32 +0600

    add string program

---
+++ 04_Type_Casting/04_Type_Casting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04_Type_Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
