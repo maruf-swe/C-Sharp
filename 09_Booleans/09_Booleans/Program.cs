419659267ca9c16ad58b2d8777e901b4344d4754
Maruf <marufur1847@gmail.com>
2021-08-16 19:51:06 +0600

    add Booleans program

---
+++ 09_Booleans/09_Booleans/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _09_Booleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boolean Values
+            bool isCSharpFun = true;
+            bool isFishTasty = false;
+            Console.WriteLine(isCSharpFun);
+            Console.WriteLine(isFishTasty);
+
+            //Boolean Expression
+            int x = 5;
+            int y = 10;
+            Console.WriteLine(x > y);
+            Console.WriteLine(10 > 9);
+
+            int myNumber = 10;
+            Console.WriteLine(myNumber == 10);
+        }
+    }
+}
