0585278ed6af2823f3c70f9e0cca9fe9c289b3a0
Maruf <marufur1847@gmail.com>
2021-08-16 15:33:37 +0600

    add operators program

--- 06_Operators/06_Operators/Program.cs
+++ 06_Operators/06_Operators/Program.cs
@@ -6,7 +6,27 @@ namespace _06_Operators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 50;
+            int y = 10;
+            int c = x + y;
+            int d = 100 + 50;
+            int e = x - y;
+            int f = x * y;
+            int g = x / y;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(x - y);
+            Console.WriteLine(f);
+            Console.WriteLine(x * y);
+            Console.WriteLine(g);
+            Console.WriteLine(x / y);
+
+            //increment
+            int p = 5;
+            p++;
+            Console.WriteLine(p);
+
         }
     }
 }
