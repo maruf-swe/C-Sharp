6f86df2db8a8bee251c1695f1edf4a4fbac09bed
Maruf <marufur1847@gmail.com>
2021-08-16 13:50:37 +0600

    add user input program

---
+++ 05_User_Input/05_User_Input/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05_User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example
+
+            // Type your username and press enter
+            Console.WriteLine("Enter username:");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
+
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is: " + userName);
+
+           
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+        }
+    }
+}
---
+++ 06_Operators/06_Operators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
