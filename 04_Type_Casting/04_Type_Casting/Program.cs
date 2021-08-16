022271045e06f68b46218283957db9dd7fe99d52
Maruf <marufur1847@gmail.com>
2021-08-16 13:28:54 +0600

    programm add

--- 04_Type_Casting/04_Type_Casting/Program.cs
+++ 04_Type_Casting/04_Type_Casting/Program.cs
@@ -6,7 +6,29 @@ namespace _04_Type_Casting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Implicit Casting
+            int myInt = 9;
+            double myDouble = myInt; // Automatic casting: int to double
+            Console.WriteLine(myInt);
+            Console.WriteLine(myDouble);
+
+            // Explicit Casting
+            double myDouble2 = 9.78;
+            int myInt2 = (int)myDouble2;    // Manual casting: double to int
+
+            Console.WriteLine(myDouble2);   // Outputs 9.78
+            Console.WriteLine(myInt2);      // Outputs 9
+
+            // Type Conversation Methods
+            int myInt3 = 10;
+            double myDouble3 = 5.25;
+            bool myBool = true;
+            int m = Convert.ToInt32(myDouble3);
+            Console.WriteLine(Convert.ToString(myInt3));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt3));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble3));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+            Console.WriteLine(m);
         }
     }
 }
